754df0d882fbf8fb368da05f15722566fa0aa58e
Bagoole-Ivan <ivanbagoole2001@gmail.com>
2023-02-28 20:26:19 +0300

    scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture Scripture = new Scripture();
+        while (true)
+        {
+            Console.Clear();
+            Scripture1 = Console.WriteLine("Moroni 10:4:And when ye shall receive these things, I would exhort you that ye would aask God, the Eternal Father, in the name of Christ, if these things are not btrue; and if ye shall ask with a csincere heart, with dreal intent, having efaith in Christ, he will fmanifest the gtruth of it unto you, by the power of the Holy Ghost.  ");
+            Console.ReadLine(Scripture1);
+            choice = Console.ReadLine("Press enter to continue or type quit to finish.")
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == enter)
+            {
+                Entry newEntry = new Entry();
+                class GFG {
+      
+                    // Main Method
+                    static public void Main()
+                    {
+                        
+                        // creating object
+                        DemoEncap Scripture1 = new DemoEncap();
+                
+                        Scripture1.Name = "Moroni 10:4: And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, He will manifest the truth of it unto you, by the power of the Holy Ghost. ";
+                        Scripture1.Console.ReadLine(Scripture1) = Random(------);
+                
+                        // Displaying values of the variables
+                        Console.WriteLine( Encap.Scripture1 + Scripture1.Console.ReadLine(Scripture1));
+                    }
+                }
+                
+            }
+            else if (choice == "quit")
+            {
+                Scripture1.quit;
+            }
+            Scripture1.SaveFile();
+            }
+            else if (choice == 5)
+            {
+                break;
+            }
+        }
+        
+
     }
-}
